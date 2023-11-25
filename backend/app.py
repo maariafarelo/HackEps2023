4f617c4120c306bd2fc1c8c9a0efb8e6b8ad9e41
AwaKT <gerardm27@gmail.com>
2023-11-25 17:59:45 +0100

    Added feedback routes structure

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/feedback', methods=['GET'])
+def get_feedback():
+    return 'Get feedback!'
+
+@app.route('/feedback', methods=['POST'])
+def post_feedback():
+    return 'Post feedback!'
+
+
+@app.route('/feedback/<int:feedback_id>', methods=['POST'])
+def send_feedback_to_asana(feedback_id):
+    return 'Send feedback to Asana!'
+
+
+
+
+    
