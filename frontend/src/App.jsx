6fdc6ec494cf62895e05e33521cc264d9c22526b
AwaKT <gerardm27@gmail.com>
2023-11-26 03:20:10 +0100

    Fixed styles frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,78 @@
-
-
-
-
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-
-
 function App() {
+  const [showToast, setShowToast] = useState(false);
+
+  const handleSubmit = () => {
+    const client = document.getElementsByName('input1')[0].value;
+    const product = document.querySelector('.selectInput').value; // Use querySelector for select
+    const feedback = document.getElementsByName('input3')[0].value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://127.0.0.1:5000/feedback');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+      client,
+      product,
+      feedback
+    }));
+
+    // Call to backend
+    setShowToast(true);
+  };
 
   return (
-  
-    
-    <div className='croqueta'>
-      
-      <div className='submitfeedback' >
-        <h1> Submit your feedback! </h1> 
-     </div>
-
-     <div className='whoareyou' >
-        <h3> Who are you?  </h3>
-     </div>
-    
-     <div className='input1' >
-     <input name="input1" />
+    <div className='mainPage'>
+      <div className='submitfeedback'>
+        <h1>Submit your feedback!</h1>
       </div>
-
-     <div className='producte' >
-       <h3> Which product do you want to tell us about?</h3>
-     </div>
-
-     <div className='input1' >
-      <input name="input1" />
+      <form>
+        <div className='whoareyou'>
+          <h3> Which client do you represent? </h3>
+        </div>
+        <div className='input1'>
+          <input name="input1" />
+        </div>
+        <div className='producte'>
+          <h3> Which product do you want to tell us about?</h3>
+        </div>
+        <div className='input1'>
+          <select className='selectInput'>
+            <option name="intech3d">Intech3D</option>
+            <option name="invelon">Invelon</option>
+            <option name="origen">Origen</option>
+            <option name="innitia">Innitia</option>
+            <option name="xrshop">XRShop</option>
+            <option name="ingamevr">InGameVR</option>
+            <option name="print&go">Print&Go</option>
+            <option name="aurora">Aurora</option>
+            <option name="fabrex">Fabrex</option>
+          </select>
+        </div>
+        <div className='proposal'>
+          <h3> Explain your feedback or request</h3>
+        </div>
+        <div className='input3'>
+          <input name="input3" />
+        </div>
+      </form>
+      <div className='button'>
+        <button onClick={handleSubmit}>SUBMIT</button>
       </div>
-
-
-     <div className='proposal' >
-       <h3> Explain your proposal</h3>
+      {showToast && (
+        <div className='toast-modal'>
+          <p>Successfully added feedback!</p>
+          <button onClick={() => {
+            setShowToast(false)
+            window.location.reload()
+          }}>Close</button>
+        </div>
+      )}
     </div>
-
-    <div className='input2' >
-     <input name="input2" />
-      </div>
-
-    
-  
-     <div className='button'>
-     <button> SUBMIT </button>
-     </div>
-     
-    
-
-    </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
